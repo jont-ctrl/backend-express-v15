274bc9550075338a7da38b3280cd7af318e3dd2b
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-04-09 09:48:16 +0200

    res header

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/api/welcome', (req, res) => {
+  res.header('Welcome-message', 'Welcome to our api');
+  res.send('Welcome');
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
